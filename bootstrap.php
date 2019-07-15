a73b3269a559a1342138fa924003ed161010cefe
David Ryan <reachdavidryan@gmail.com>
2019-07-15 15:26:39 -0400

    Initla Gutenframe Commit

---
+++ bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+if ( function_exists( 'add_action' ) ) {
+	add_action( 'after_theme_setup', 'eig_module_gutenframe_register' );
+}
+/**
+ * Register endurance-wp-module-gutenframe
+ *
+ * @return void
+ */
+function eig_module_gutenframe_register() {
+	eig_register_module(
+		array(
+			'name'     => 'gutenframe',
+			'label'    => __( 'Gutenframe', 'endurance' ),
+			'callback' => 'eig_module_gutenframe_load',
+			'isActive' => true,
+			'isHidden' => true,
+		)
+	);
+
+	if ( ! defined( 'EIG_GUTENFRAME_PYM_URL' ) ) {
+		define(
+			'EIG_GUTENFRAME_PYM_URL',
+			trailingslashit( MM_BASE_URL ) . 'vendor/bluehost/endurance-wp-module-gutenframe/assets/pym.js'
+		);
+	}
+}
+
+/**
+ * Instantiate endurance-wp-module-gutenframe main class.
+ *
+ * @return void
+ */
+function eig_module_gutenframe_load() {
+	if ( ! class_exists( 'EIG_Module_Gutenframe' ) ) {
+		new EIG_Module_Gutenframe();
+	}
+}
